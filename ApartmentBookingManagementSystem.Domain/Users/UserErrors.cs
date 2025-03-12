4af7a963b8c1682a081f267765763632531c8e8f
guiller santos <guillersantos111@gmail.com>
2025-03-12 14:33:11 +0800

    Added UserError static class for User Error Response

---
+++ ApartmentBookingManagementSystem.Domain/Users/UserErrors.cs
@@ -0,0 +1,22 @@
+﻿using ApartmentBookingManagementSystem.Domain.Abstractions;
+
+namespace ApartmentBookingManagementSystem.Domain.Users;
+
+public static class UserErrors
+{
+    #region Fields
+
+    public static Error NotFound = new
+        (
+            "User.Found",
+            "The User With The Specified Identifier Was Not Found"
+        );
+
+    public static Error InvalidCredentials = new
+        (
+            "User.InvalidCredentials",
+            "The Provided Credentials Were Invalid"
+        );
+
+    #endregion Fields
+}
