0c2b5cd2cd9fdc8d95d04d4afa7cf52b308df748
guiller santos <guillersantos111@gmail.com>
2025-03-12 14:29:53 +0800

    Added ApartmentErrors for Apartment Error Response

---
+++ ApartmentBookingManagementSystem.Domain/Apartments/ApartmentErrors.cs
@@ -0,0 +1,16 @@
+﻿using ApartmentBookingManagementSystem.Domain.Abstractions;
+
+namespace ApartmentBookingManagementSystem.Domain.Apartments;
+
+public static class ApartmentErrors
+{
+    #region Fields
+
+    public static Error NotFound = new
+        (
+            "Property.Found",
+            "The Property With The Specified Identifier Was Not Found"
+        );
+
+    #endregion Fields
+}
