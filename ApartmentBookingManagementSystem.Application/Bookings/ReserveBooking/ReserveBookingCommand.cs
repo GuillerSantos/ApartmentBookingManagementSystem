ee08647e1df736521720097ebb825e98fd1770d7
guiller santos <guillersantos111@gmail.com>
2025-03-12 14:28:53 +0800

    Added Booking sealed class for Booking Response

---
+++ ApartmentBookingManagementSystem.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,11 @@
+﻿using ApartmentBookingManagementSystem.Application.Abstractions.Messaging;
+
+namespace ApartmentBookingManagementSystem.Application.Bookings.ReserveBooking;
+
+public record ReserveBookingCommand
+    (
+        Guid ApartmentId,
+        Guid UserId,
+        DateOnly StartDate,
+        DateOnly EndDate
+    ) : ICommad<Guid>;
