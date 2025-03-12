48d0f611dda6d08288ddb4e736de67b961e8d905
guiller santos <guillersantos111@gmail.com>
2025-03-12 14:32:13 +0800

    Added IsOverlappingAsync in IBookingRepo Interface

---
+++ ApartmentBookingManagementSystem.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApartmentBookingManagementSystem.Domain.Apartments;
+
+namespace ApartmentBookingManagementSystem.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    #region Public Methods
+
+    Task<Booking> IsOverlappingAsync
+        (
+            Apartment apartment,
+            DateRange duration,
+            CancellationToken cancellationToken = default
+        );
+
+    void Add(Booking booking);
+
+    #endregion Public Methods
+}
